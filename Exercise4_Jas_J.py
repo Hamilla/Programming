0447126e64fa05d9b5e2108f18cc2671e0b1175f
Jas Jaidee <52348549+Hamilla@users.noreply.github.com>
2019-07-28 21:55:07 +0700

    Add files via upload

---
+++ Exercise4_Jas_J.py
@@ -0,0 +1,13 @@
+a="Foundation English:"
+FE=70
+b="General Business:"
+GB=65
+c="Introduction to Computer Systems:"
+ICS=60
+d="Computer Progreamming:"
+CP=80
+print("----Your Score----")
+print(a,FE)
+print(b,GB)
+print(c,ICS)
+print(d,CP)
